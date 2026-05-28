4bd0d7ab945bd23d472e4a589f18c9995ccafe88
drahciry <richardgonric@gmail.com>
2026-05-28 15:22:39 -0300

    add: minimum common value in two sorted arrays solved with two pointers approach

---
+++ Array/leetcode_2540.c
@@ -0,0 +1,20 @@
+/**
+ * Problem: Minimum Common Value (LeetCode 2540)
+ * Time Complexity: O(N + M) - N is the nums1 size and M is the nums2 size
+ * Memory Complexity: O(1)
+ */
+
+int getCommon(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    int i = 0;
+    int j = 0;
+
+    while (i < nums1Size && j < nums2Size)
+        if (nums1[i] == nums2[j])
+            return nums1[i];
+        else if (nums1[i] < nums2[j])
+            i++;
+        else
+            j++;
+    
+    return -1;
+}
